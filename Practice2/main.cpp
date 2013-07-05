0f6cfd60ed8365dc547903158f3a43b6e47590a6
Ali-J <alastair_jarrett@live.co.uk>
2013-07-05 20:34:11 +0100

    upload new changers

--- Practice2/main.cpp
+++ Practice2/main.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-  cout<<"HEY, you, I'm alive! Oh, and Hello World!\n";
-  cin.get();
+    int thisisanumber;
+
+    cout<<"Please enter a number: ";
+    cin>> thisisanumber;
+    cin.ignore();
+    if(thisisanumber > 12)
+        {
+        cout<< thisisanumber <<" is greater then 12\n";
+        cin.get();
+        }
+    else
+        {
+        cout<< thisisanumber <<" is less then 12\n";
+        cin.get();
+        }
 }

8c3188883cf3c62231809afe1c8f0328fc34bab4
Ali-J <alastair_jarrett@live.co.uk>
2013-07-05 21:01:14 +0100

    uploaded additional code

--- Practice2/main.cpp
+++ Practice2/main.cpp
@@ -11,12 +11,19 @@ int main()
     cin.ignore();
     if(thisisanumber > 12)
         {
-        cout<< thisisanumber <<" is greater then 12\n";
+        //Check if variable is greater then 12
+        cout<< thisisanumber <<" is greater than 12\n";
         cin.get();
         }
+    else if(thisisanumber == 12)
+        {
+        //Check if variable is equal to 12
+        cout<< thisisanumber <<" is equal to 12\n";
+        cin.get();}
     else
         {
-        cout<< thisisanumber <<" is less then 12\n";
+        //Check if variable is less than 12
+        cout<< thisisanumber <<" is less than 12\n";
         cin.get();
         }
 }
